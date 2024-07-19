3a0dad09c71c5977bf24333971cf850e988f4ebf
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-07-19 22:22:39 +0300

    + connected mongo database

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
+import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
+import connectDB from './config/db';
 
 const app = express();
 
 const PORT = 3000;
 
-
+connectDB()
+;
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
 })
