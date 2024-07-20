975ce809aa69a627e1b578e3b18798739a9bc942
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-07-20 21:02:06 +0300

    + cors and json middleware

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import connectDB from './config/db';
 import { requestLogger } from "./middleware/request-logger.middleware";
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3000;
 
 connectDB();
+
+app.use(cors());
+app.use(express.json());
 
 app.use(requestLogger)
 
