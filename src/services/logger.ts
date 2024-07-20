b5acb39ed880a2e64f5d51e6ca1e8902fdfe2087
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-07-20 15:32:07 +0300

    + pino and logger service

---
+++ src/services/logger.ts
@@ -0,0 +1,49 @@
+import { type Logger as LibraryLogger, pino } from 'pino';
+import pretty from 'pino-pretty';
+
+// import { type Logger } from './libs/types/types.js';
+
+type LogFunction = (
+    message: string,
+    parameters?: Record<string, unknown>,
+  ) => void;
+  
+interface ILogger {
+    info: LogFunction;
+    warn: LogFunction;
+    error: LogFunction;
+};
+
+
+class LoggerService implements ILogger {
+  private logger: LibraryLogger;
+
+  public constructor() {
+    this.logger = pino();
+
+    this.logger.info('Logger is created…');
+  }
+
+  public error(
+    message: string,
+    parameters: Record<string, unknown> = {},
+  ): void {
+    this.logger.error(parameters, message);
+  }
+
+  public info(
+    message: string,
+    parameters: Record<string, unknown> = {},
+  ): void {
+    this.logger.info(parameters, message);
+  }
+
+  public warn(
+    message: string,
+    parameters: Record<string, unknown> = {},
+  ): void {
+    this.logger.warn(parameters, message);
+  }
+}
+
+export { LoggerService };
